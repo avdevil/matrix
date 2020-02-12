b7f68aad79a2c374730f4e1df3355a196bcc3c0b
avdevil <60883474+avdevil@users.noreply.github.com>
2020-02-12 19:11:57 +0530

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -5,10 +5,10 @@ a = np.matrix([[1, 2], [3, 4]])  # using array of array
 print('2x2 matrix is:\n', a)
 print('The dimension of the matrix is :', a.shape)
 
-b = np.matrix('[1,2;3,4;5,6]', dtype=np.int32)  # limiting the data-type to int
+b = np.matrix('[1,2;3,4;5,6]', dtype=np.int32)  
 print('\n3x2 matrix is:\n', b)
 print('The dimension of the matrix is :', b.shape)
 
-c = np.matrix(np.random.rand(3, 3), dtype=np.float32)  # considering the data-type as float
+c = np.matrix(np.random.rand(3, 3), dtype=np.float32) 
 print('\n3x3 random element matrix is:\n', c)
 print('The dimension of the matrix is :', c.shape)
