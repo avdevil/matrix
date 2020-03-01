d1a60bef2fd6d167ed1490fa3378fad7237da804
avdevil <60883474+avdevil@users.noreply.github.com>
2020-03-01 09:23:01 +0530

    Update Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,18 @@
 # Python Program - Make Simple Calculator
 import requests
-for i in range(10):
-print("coded by avdevil")
-for i in range(1):
+import banner
+def banner():
+    print(r"                       __  __ ___ _  _ ___                        ")
+    print(r"                      |  \/  |_ _| \| |_ _|                       ")
+    print(r"                      | |\/| || || .\`|| |                        ")
+    print(r"                      |_|  |_|___|_|\_|___|                       ")
+    print(r"   _____ __  __  _____   ____   ____  __  __ ____  ______ _____   ")
+    print(r"  / ____|  \/  |/ ____| |  _ \ / __ \|  \/  |  _ \|  ____|  __ \  ")
+    print(r" | (___ | \  / | (___   | |_) | |  | | \  / | |_) | |__  | |__) | ")
+    print(r"  \___ \| |\/| |\___ \  |  _ <| |  | | |\/| |  _ <|  __| |  _  /  ")
+    print(r"  ____) | |  | |____) | | |_) | |__| | |  | | |_) | |____| | \ \  ")
+    print(r" |_____/|_|  |_|_____/  |____/ \____/|_|  |_|____/|______|_|  \_\ ")
+    print(r"                                                                  ")
 print("1. Addition");
 print("2. Subtraction");
 print("3. Multiplication");
@@ -29,3 +39,4 @@ elif choice == 5:
     exit();
 else:
     print("Wrong input..!!");
+banner():
